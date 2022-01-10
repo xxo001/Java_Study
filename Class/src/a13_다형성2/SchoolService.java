e82b9cc79f7ff9d5621dd7daad7a714ebd996fce
DESKTOP-50OEOTF\gsr09 <gsr0920@naver.com>
2022-01-11 01:26:20 +0900

    <8-13> 반복문 - 다형성

--- Class/src/a13_다형성2/SchoolService.java
+++ Class/src/a13_다형성2/SchoolService.java
@@ -20,33 +20,72 @@ public class SchoolService {
 		return new Person[index];
 	}
 	
-	public void addTeacher() {
+	public  Teacher addTeacher() {
 		Teacher teacher = new Teacher();
 		System.out.println("선생님 정보를 입력해 주세요");
 		System.out.print("이름 : ");
-		teacher.setName(scanner.nextLine());
-		System.out.print("담당과목 : ");
+		teacher.setName(scanner.next());
+		scanner.nextLine();
+		System.out.print("과목 : ");
 		teacher.setSubject(scanner.nextLine());
-		System.out.println("");
+		System.out.print("담당학년 : ");
+		teacher.setClassYear(scanner.nextInt());
+		scanner.nextLine();
+		System.out.print("담당반 : ");
+		teacher.setClassGroup(scanner.nextInt());
+		scanner.nextLine();
+		
+		return teacher;
+		
+	}
+	
+	public Student addStudent () {
+		Student student = new Student();
+		System.out.println("학생 정보를 입력해 주세요");
+		System.out.print("이름 : ");
+		student.setName(scanner.next());
+		System.out.print("학년 : ");
+		student.setStudentYear(scanner.nextInt());
+		scanner.nextLine();
+		System.out.print("반 : ");
+		student.setStudentGroup(scanner.nextInt());
+		scanner.nextLine();
+		System.out.print("번호 : ");
+		student.setStudentNumber(scanner.nextInt());
+		scanner.nextLine();
+		
+		return student;
 		
 		
 	}
 	
-	public void addStudent () {
-		
-	}
-	
-	public void insertPerson(Person[] person) {
+	public void insertPerson(Person[] persons) {
 		/*
 		 *  1. 학생등록
 		 *  2. 선생님등록
 		 *  if 1이면 학생등록
 		 *  if 2면 선생님 등록
 		 */
-		
+		for(int i = 0 ; i<persons.length;i++) {
+			System.out.print("학생등록(1) / 선생님등록(2) : ");
+			int num = scanner.nextInt();
+			
+			if(num == 1) {
+				persons[i] = addStudent();
+			}
+			else {
+				persons[i] = addTeacher();
+			}
+			
+		}
+		System.out.println("학생/선생님의 정보 입력 완료");
 	}
 	
-	public void showInfoPerson(Person[] person) {
+	public void showInfoPerson(Person[] persons) {
+		for(int i=0;i<persons.length;i++) {
+			
+			persons[i].showInfo();
+		}
 		
 	}
 }
